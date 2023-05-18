1180512deddeabc939107655d3f7007be62beb21
jolanda <j.bregu@atrax.al>
2023-05-18 15:30:28 +0200

    get users list

--- src/components/UsersTable.vue
+++ src/components/UsersTable.vue
@@ -6,7 +6,26 @@ tableeeee
 
 <script>
 export default {
-  name: "UsersTable"
+  name: "UsersTable",
+  data() {
+    return {
+      usersList: []
+    }
+  },
+  async created() {
+    await this.getUsersList();
+  },
+  methods: {
+    async getUsersList() {
+      await fetch('https://jsonplaceholder.typicode.com/users/')
+          .then(response => response.json())
+          .then(data => {
+            this.usersList = data;
+          }).catch(err => {
+            console.error(err);
+          })
+    }
+  }
 }
 </script>
 
