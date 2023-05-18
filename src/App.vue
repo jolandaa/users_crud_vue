d722d169b98791c5a5c602d3bab4129b8054c3d5
jolanda <j.bregu@atrax.al>
2023-05-18 11:48:47 +0200

    create vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <UsersTable />
+</template>
+
+<script>
+import UsersTable from './components/UsersTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UsersTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+tableeeee
+</div>
+</template>
+
+<script>
+export default {
+  name: "UsersTable"
+}
+</script>
+
+<style scoped>
+
+</style>
